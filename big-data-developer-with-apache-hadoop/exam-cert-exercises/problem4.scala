e3a1d1903b8bdf9f19a96d976131206d419b34bb
Chema <josem32832@gmail.com>
2019-04-23 11:16:02 +0200

    Done again

---
+++ big-data-developer-with-apache-hadoop/exam-cert-exercises/problem4.scala
@@ -0,0 +1,71 @@
+/*
+Question 4: Correct
+Prerequiste:
+[Prerequisite section will not be there in actual exam]
+
+Import orders table from mysql into hdfs location /user/cloudera/practice4/question3/orders/.Run below sqoop statement
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+--username root \
+--password cloudera \
+--table orders \
+--target-dir /user/cloudera/practice4/question3/orders/
+
+Import customers from mysql into hdfs location /user/cloudera/practice4/question3/customers/.Run below sqoop statement
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+--username root \
+--password cloudera \
+--table customers \
+--target-dir /user/cloudera/practice4/question3/customers/ \
+--columns "customer_id,customer_fname,customer_lname"
+
+Instructions:
+
+Join the data at hdfs location /user/cloudera/practice4/question3/orders/ & /user/cloudera/practice4/question3/customers/ to find out customers whose orders status is like "pending"
+Schema for customer File
+Customer_id,customer_fname,customer_lname
+Schema for Order File
+Order_id,order_date,order_customer_id,order_status
+
+Output Requirement:
+Output should have customer_id,customer_fname,order_id and order_status.Result should be saved in /user/cloudera/p1/q7/output
+*/
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+  --username root \
+  --password cloudera \
+  --table orders \
+  --delete-target-dir \
+  --target-dir /user/cloudera/practice4/question3/orders/ \
+--outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+  --username root \
+  --password cloudera \
+  --table customers \
+  --delete-target-dir \
+  --target-dir /user/cloudera/practice4/question3/customers/ \
+--columns "customer_id,customer_fname,customer_lname" \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
+
+val orders = sc.textFile("/user/cloudera/practice4/question3/orders/").map(line => line.split(",")).map(arr => (arr(0).toInt,arr(1),arr(2).toInt,arr(3))).toDF("order_id","order_date","order_customer_id","order_status")
+val customer = sc.textFile("/user/cloudera/practice4/question3/customers/").map(line => line.split(",")).map(arr => (arr(0),arr(1),arr(2))).toDF("customer_id","customer_fname","customer_lname")
+
+orders.registerTempTable("o")
+customer.registerTempTable("c")
+
+sqlContext.sql("""select customer_id,customer_fname,order_id,order_status from c join o on(c.customer_id = o.order_customer_id) where order_status like("%PENDING%")""").show(10)
+val result = sqlContext.sql("""select customer_id,customer_fname,order_id,order_status from c join o on(c.customer_id = o.order_customer_id) where order_status like("%PENDING%")""")
+
+result.repartition(1).rdd.map(r => r.mkString(",")).saveAsTextFile("/user/cloudera/p1/q7/output")
+
+$ hdfs dfs -ls /user/cloudera/p1/q7/output
+$ hdfs dfs -cat /user/cloudera/p1/q7/output/part-00000 | head -n 20
---
+++ big-data-developer-with-apache-hadoop/exam-cert-exercises/problem5.scala
@@ -0,0 +1,44 @@
+/*
+Question 5: Correct
+PreRequiste:
+[Prerequisite section will not be there in actual exam]
+Run below sqoop command to import customer table from mysql into hdfs to the destination /user/cloudera/problem5/customer/parquet as parquet file. Only import customer_id,customer_fname,customer_city.
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+--password cloudera \
+--username root \
+--table customers \
+--columns "customer_id,customer_fname,customer_city" \
+--target-dir /user/cloudera/problem5/customer/parquet \
+--as-parquetfile
+
+Instructions:
+
+Count number of customers grouped by customer city and customer first name where customer_fname is like "Mary" and order the results by customer first name and save the result as text file.
+Input folder is /user/cloudera/problem5/customer/parquet.
+
+Output Requirement:
+Result should have customer_city,customer_fname and count of customers and output should be saved in /user/cloudera/problem5/customer_grouped as text file with fields separated by pipe character
+*/
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+  --password cloudera \
+  --username root \
+  --table customers \
+  --columns "customer_id,customer_fname,customer_city" \
+  --delete-target-dir \
+  --target-dir /user/cloudera/problem5/customer/parquet \
+  --as-parquetfile \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
+
+val customer = sqlContext.read.parquet("/user/cloudera/problem5/customer/parquet")
+customer.registerTempTable("customer")
+sqlContext.sql("""select customer_city, customer_fname, count(customer_id) as total_customers from customer where customer_fname like("%Mary%") group by customer_city,customer_fname order by customer_fname""").show(10)
+val result = sqlContext.sql("""select customer_city, customer_fname, count(customer_id) as total_customers from customer where customer_fname like("%Mary%") group by customer_city,customer_fname order by customer_fname""")
+result.repartition(1).rdd.map(r => r.mkString("|")).saveAsTextFile("/user/cloudera/problem5/customer_grouped")
+
+$ hdfs dfs -ls /user/cloudera/problem5/customer_grouped
+$ hdfs dfs -cat /user/cloudera/problem5/customer_grouped/part-00000 | tail -n 20
