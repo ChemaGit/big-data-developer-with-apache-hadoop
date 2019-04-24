79ffe0f6406463026042ded4f5155f407ce05e16
Chema <josem32832@gmail.com>
2019-04-24 13:24:33 +0200

    commit it's done

---
+++ big-data-developer-with-apache-hadoop/exam-cert-exercises/problem6.scala
@@ -0,0 +1,47 @@
+/*
+Question 6: Correct
+PreRequiste:
+[Prerequisite section will not be there in actual exam]
+Run below sqoop command to import customer table from mysql into hdfs to the destination /user/cloudera/problem6/customer/text as text file and fields seperated by tab character Only import customer_id,customer_fname,customer_city.
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+--password cloudera \
+--username root \
+--table customers \
+--fields-terminated-by '\t' \
+--columns "customer_id,customer_fname,customer_city" \
+--target-dir /user/cloudera/problem6/customer/text
+
+Instructions:
+Find all customers that lives 'Brownsville' city and save the result into HDFS.
+Input folder is /user/cloudera/problem6/customer/text.
+
+Output Requirement:
+Result should be saved in /user/cloudera/problem6/customer_Brownsville Output file should be saved in Json format
+
+[You will not be provided with any answer choice in actual exam.Below answers are just provided to guide you]
+Important Information:
+Please make sure you are running all your solutions on spark 1.6 since it will be default spark version provided by exam environment.
+*/
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+  --password cloudera \
+  --username root \
+  --table customers \
+  --fields-terminated-by '\t' \
+  --columns "customer_id,customer_fname,customer_city" \
+  --delete-target-dir \
+  --target-dir /user/cloudera/problem6/customer/text \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
+
+case class Customer(id: Int,name: String,city: String)
+val customers = sc.textFile("/user/cloudera/problem6/customer/text").map(line => line.split('\t')).map(r => Customer(r(0).toInt,r(1),r(2))).toDF
+customers.show(10)
+val custFilter = customers.filter("city = 'Brownsville'")
+custFilter.show(10)
+custFilter.repartition(1).toJSON.saveAsTextFile("/user/cloudera/problem6/customer_Brownsville")
+
+$ hdfs dfs -cat /user/cloudera/problem6/customer_Brownsville/part-00000 | head -n 50
---
+++ big-data-developer-with-apache-hadoop/exam-cert-exercises/problem7.scala
@@ -0,0 +1,45 @@
+/*
+Question 7: Correct
+PreRequiste:
+[Prerequisite section will not be there in actual exam]
+Run below sqoop command to import few columns from customer table from mysql into hdfs to the destination /user/cloudera/practice1/problem7/customer/avro_snappy as avro file.
+
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+--password cloudera \
+--username root \
+--table customers \
+--target-dir /user/cloudera/practice1/problem7/customer/avro \
+--columns "customer_id,customer_fname,customer_lname" --as-avrodatafile
+
+Instructions:
+Convert data-files stored at hdfs location /user/cloudera/practice1/problem7/customer/avro into tab delimited file using gzip compression and save in HDFS.
+
+Output Requirement:
+Result should be saved in /user/cloudera/practice1/problem7/customer_text_gzip Output file should be saved as tab delimited file in gzip Compression.
+
+Sample Output:
+
+21 Andrew Smith
+111 Mary Jons
+*/
+sqoop import \
+--connect "jdbc:mysql://localhost/retail_db" \
+  --password cloudera \
+  --username root \
+  --table customers \
+  --delete-target-dir \
+  --target-dir /user/cloudera/practice1/problem7/customer/avro \
+  --columns "customer_id,customer_fname,customer_lname" \
+  --as-avrodatafile \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
+
+import com.databricks.spark.avro._
+val customers = sqlContext.read.avro("/user/cloudera/practice1/problem7/customer/avro")
+customers.show()
+customers.rdd.map(r => r.mkString("\t")).repartition(1).saveAsTextFile("/user/cloudera/practice1/problem7/customer_text_gzip", classOf[org.apache.hadoop.io.compress.GzipCodec])
+
+$ hdfs dfs -ls /user/cloudera/practice1/problem7/customer_text_gzip
+$ hdfs dfs -text /user/cloudera/practice1/problem7/customer_text_gzip/part-00000.gz
