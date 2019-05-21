1e302b26ec839446586130c858a8d8a42701a684
Chema <josem32832@gmail.com>
2019-05-21 16:17:38 +0200

    21052019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-2/question41.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-2/question41.scala
@@ -1,46 +1,20 @@
-/**
- * Problem Scenario 70 : Write down a Spark Application using Python, In which it read a
- * file "content.txt" (On hdfs) with following content. Do the word count and save the
- * results in a directory called "problem85" (On hdfs)
- * content.txt
- * Hello this is ABCTECH.com
- * This is XYZTECH.com
- * Apache Spark Training
- * This is Spark Learning Session
- * Spark is faster than MapReduce 
- */
+/** Question 41
+  * Problem Scenario 70 : Write down a Spark Application using Scala, In which it read a
+  * file "Content.txt" (On hdfs) with following content. Do the word count and save the
+  * results in a directory called "question41" (On hdfs)
+  * content.txt
+  * Hello this is ABCTECH.com
+  * This is XYZTECH.com
+  * Apache Spark Training
+  * This is Spark Learning Session
+  * Spark is faster than MapReduce
+  */
+// Scala
+val filtered = List("", " ")
+val content = sc.textFile("/user/cloudera/files/Content.txt").flatMap(line => line.split("\\W")).filter(w => !filtered.contains(w))
+val result = content.map(w => (w, 1)).reduceByKey( (v, c) => v + c).sortBy(t => t._2, false)
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+result.repartition(1).saveAsTextFile("/user/cloudera/question41/result")
 
-//Explanation: Solution : 
-//Step 1 : Create an application with following code and store it in problem84.py 
-# Import SparkContext and SparkConf from pyspark 
-from pyspark import SparkContext, SparkConf 
-# Create configuration object and set App name 
-conf = SparkConf().setAppName("CCA 175 Problem 85") 
-sc = SparkContext(conf=conf) 
-#load data from hdfs 
-contentRDD = sc.textFile("/files/content.txt") 
-#filter out non-empty lines 
-nonemptylines = contentRDD.filter(lambda x: len(x) > 0) 
-#Split line based on space 
-words = nonemptylines.flatMap(lambda x: x.split(' ')) 
-#Do the word count 
-wordcounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x+y).map(lambda x: (x[1], x[0])).sortByKey(False) 
-for word in wordcounts.collect(): print(word) 
-#Save final data " 
-wordcounts.repartition(1).saveAsTextFile("/files/problem85") 
-#Stop Sparks
-sc.stop()
-
-//step 2 : Submit this application 
-spark-submit --master yarn problem84.py
-
-//Solution in pyspark
-$ gedit /home/training/Desktop/files/content.txt &
-$ hdfs dfs -put /home/training/Desktop/files/content.txt /files
-
-rdd = sc.textFile("/files/content.txt").flatMap(lambda line: line.split(' '))
-rrdCount = rdd.map(lambda word: (word, 1)).reduceByKey(lambda v, v1: v + v1)
-rddCount.collect()
-rddCount.repartition(1).saveAsTextFile("/files/problem85")
+$ hdfs dfs -ls hdfs dfs -ls /user/cloudera/question41/result/
+  $ hdfs dfs -cat /user/cloudera/question41/result/part-*
--- big-data-developer-with-apache-hadoop/training-cert-exercises-2/question42.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-2/question42.scala
@@ -1,52 +1,59 @@
-/**
- * Problem Scenario 85 : In Continuation of previous question, please accomplish following activities.
- * 1. Select all the columns from product table with output header as below. productID AS ID code AS Code name AS Description price AS 'Unit Price'
- * 2. Select code and name both separated by '-' and header name should be 'Product Description'.
- * 3. Select all distinct prices.
- * 4. Select distinct price and name combination.
- * 5. Select all price data sorted by both code and productID combination.
- * 6. count number of products.
- * 7. Count number of products for each code.
- */
-//Solution with SPARK
-//Answer : See the explanation for Step by Step Solution and configuration.
+/** Question 42
+productID,code,name,quantity,price,idSub
+1001,PEN,Pen Red,5000,1.23,501
+1002,PEN,Pen Blue,8000,1.25,501
+1003,PEN,Pen Black,2000,1.25,501
+1004,PEC,Pencil 2B,10000,0.48,502
+1005,PEC,Pencil 2H,8000,0.49,502
+1006,PEC,Pencil HB,0,9999.99,502
+2001,PEC,Pencil 3B,500,0.52,501
+2002,PEC,Pencil 4B,200,0.62,501
+2003,PEC,Pencil 5B,100,0.73,501
+2004,PEC,Pencil 6B,500,0.47,502
+  * Problem Scenario 85 : In Continuation of previous question, please accomplish following activities.
+  * 1. Select all the columns from product table with output header as below. productID AS ID code AS Code name AS Description price AS 'Unit Price'
+  * 2. Select code and name both separated by '-' and header name should be 'Product Description'.
+  * 3. Select all distinct prices.
+  * 4. Select distinct price and name combination.
+  * 5. Select all price data sorted by both code and productID combination.
+  * 6. count number of products.
+  * 7. Count number of products for each code.
+  */
 
-//Explanation: Solution : 
-$ spark-shell
-scala> sqlContext.sql("""show databases""")
-scala> sqlContext.sql("""use default""")
-//Step 1 : Select all the columns from product table with output header as below. productID AS ID code AS Code name AS Description price AS "Unit Price' 
-val results = sqlContext.sql("""SELECT productid AS ID, code AS Code, name AS Description, price AS `Unit Price` FROM products ORDER BY ID""") 
-results.show() 
-//Step 2 : Select code and name both separated by ' -' and header name should be "Product Description. 
-val results = sqlContext.sql("""SELECT CONCAT(code,'-', name) AS `Product Description`, price FROM products""" ) 
-results.show() 
-//Step 3 : Select all distinct prices. 
-val results = sqlContext.sql("""SELECT DISTINCT price AS `Distinct Price` FROM products""") 
-results.show() 
-//Step 4 : Select distinct price and name combination. 
-val results = sqlContext.sql("""SELECT DISTINCT price, name FROM products""" ) 
-results. show() 
-//Step 5 : Select all price data sorted by both code and productID combination. 
-val results = sqlContext.sql("""SELECT price FROM products ORDER BY code, productid""") 
-results.show() 
-//Step 6 : count number of products. 
-val results = sqlContext.sql("""SELECT COUNT(productid) AS `Count` FROM products""") 
-results.show() 
-//Step 7 : Count number of products for each code. 
-val results = sqlContext.sql("""SELECT code, COUNT(productid) as `Product Id` FROM products GROUP BY code""") 
-results.show() 
-val results = sqlContext.sql("""SELECT code, COUNT(productid) AS count FROM products GROUP BY code ORDER BY count DESC""") 
-results.show()
-//Solution with HIVE
+// SPARK SOLUTION
+val product = sc.textFile("/user/cloudera/files/product.csv").map(lines => lines.split(",")).filter(r => r(0) != "productID").map(r => (r(0).toInt,r(1),r(2),r(3).toInt,r(4).toFloat,r(5).toInt)).toDF("productID","code","name","quantity","price","idSub")
+
+product.registerTempTable("pr")
+
+// 1. Select all the columns from product table with output header as below. productID AS ID code AS Code name AS Description price AS 'Unit Price'
+sqlContext.sql("""SELECT productID as ID,code as Code,name as Description, price as `Unit Price` FROM pr""").show()
+
+// 2. Select code and name both separated by '-' and header name should be 'Product Description'.
+sqlContext.sql("""SELECT concat(code,"-",name) AS `Product Description` FROM pr""").show()
+
+// 3. Select all distinct prices.
+sqlContext.sql("""SELECT DISTINCT(price) FROM pr""").show()
+
+// 4. Select distinct price and name combination.
+sqlContext.sql("""SELECT DISTINCT(name), price FROM pr""").show()
+
+// 5. Select all price data sorted by both code and productID combination.
+sqlContext.sql("""SELECT productID, code, price from pr ORDER BY productID, code""").show()
+
+// 6. count number of products.
+sqlContext.sql("""SELECT COUNT(productID) as `Total Products` FROM pr""").show()
+
+// 7. Count number of products for each code.
+sqlContext.sql("""SELECT code, count(productID) FROM pr GROUP BY code""").show()
+
+//HIVE SOLUTION
 $ hive
-hive> show databases;
-hive> use default;
+  hive> use pruebas;
 hive> show tables;
-hive> select productid as ID, code as Code, name as Description, quantity, price as `Unit Price` from products;
-hive> select concat(code,'-' ,name) as `Product Description` from products;
-hive> select distinct price from products;
-hive> select distinct price, name from products;
-hive> select code, productid, price from products order by code, productid desc;
-hive> select count(productid) from products;
-hive> select productid, count(productid) from products group by productid;
+hive> SELECT productid as ID,productcode as Code,name as Description, price as `Unit Price` FROM t_product_parquet;
+hive> SELECT concat(productcode,"-",name) AS `Product Description` FROM t_product_parquet;
+hive> SELECT DISTINCT(price) FROM t_product_parquet;
+hive> SELECT DISTINCT(name), price FROM t_product_parquet;
+hive> SELECT productid, productcode, price from t_product_parquet ORDER BY productid, productcode;
+hive> SELECT COUNT(productid) as `Total Products` FROM t_product_parquet;
+hive> SELECT productcode, count(productid) FROM t_product_parquet GROUP BY productcode;
