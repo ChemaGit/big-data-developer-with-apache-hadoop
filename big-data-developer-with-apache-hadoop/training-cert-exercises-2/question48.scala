e002715089a63b60fa3e1a1612772d8a5c0e2810
Chema <josem32832@gmail.com>
2019-05-27 15:26:48 +0200

    27052019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-2/question48.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-2/question48.scala
@@ -1,94 +1,67 @@
-/**
- * Problem Scenario 76 : You have been given MySQL DB with following details.
- * user=retail_dba
- * password=cloudera
- * database=retail_db
- * table=retail_db.orders
- * table=retail_db.order_items
- * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
- * Columns of order table : (orderid , order_date , ordercustomerid, order_status)
- * Columns of order_items table : (order_item_id , order_item_order_id ,order_item_product_id, order_item_quantity,order_item_subtotal,order_item_product_price)
- * Please accomplish following activities.
- * 1. Copy "retail_db.orders" table to hdfs in a directory p91_orders.
- * 2. Once data is copied to hdfs, using pyspark calculate the number of order for each status.
- * 3. Use all the following methods to calculate the number of order for each status. (You need to know all these functions and its behavior for real exam)
- * -countByKey()
- * -groupByKey()
- * -reduceByKey()
- * -aggregateByKey()
- * -combineByKey()
- */
-
-//Answer : See the explanation for Step by Step Solution and configuration.
-
-//Explanation: Solution : 
-//Step 1 : Import Single table 
-$ sqoop import \
+/** Question 48
+  * Problem Scenario 76 : You have been given MySQL DB with following details.
+  * user=retail_dba
+  * password=cloudera
+  * database=retail_db
+  * table=retail_db.orders
+  * table=retail_db.order_items
+  * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
+  * Columns of order table : (orderid , order_date , ordercustomerid, order_status)
+  * Columns of order_items table : (order_item_id , order_item_order_id ,order_item_product_id, order_item_quantity,order_item_subtotal,order_item_product_price)
+  * Please accomplish following activities.
+  * 1. Copy "retail_db.orders" table to hdfs in a directory question48/orders
+  * 2. Once data is copied to hdfs, using spark-shell calculate the number of order for each status.
+  * 3. Use all the following methods to calculate the number of order for each status. (You need to know all these functions and its behavior for real exam)
+  * -countByKey()
+  * -groupByKey()
+  * -reduceByKey()
+  * -aggregateByKey()
+  * -combineByKey()
+  */
+sqoop import \
 --connect jdbc:mysql://quickstart:3306/retail_db \
---username retail_dba \
---password cloudera \
---table orders \
---delete-target-dir \
---target-dir p91_orders \
+  --username root \
+  --password cloudera \
+  --table orders \
+  --as-textfile \
+  --delete-target-dir \
+  --target-dir /user/cloudera/question48/orders \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
 --num-mappers 1
 
-//Step 2 : Read the data from one of the partition, created using above command, 
-$ hadoop fs -cat p91_orders/part-m-00000 
+val orders = sc.textFile("/user/cloudera/question48/orders").map(line => line.split(",")).map(r => (r(3), 1))
+val countByKey = orders.countByKey()
+countByKey.foreach(println)
+sc.parallelize(countByKey.toList).repartition(1).saveAsTextFile("/user/cloudera/question48/countByKey")
 
-//(These are very important functions for real exam)
-//Step 3: countByKey #Number of orders by status 
-allOrders = sc.textFile("p91_orders") 
-//#Generate key and value pairs (key is order status and value as an empty string 
-keyValue = allOrders.map(lambda line: (line.split(",")[3], "")) 
-//#Using countByKey, aggregate data based on status as a key 
-output = keyValue.countByKey().items()
-for line in output: print(line) 
+val groupByKey = orders.groupByKey().mapValues(v => v.size)
+groupByKey.collect.foreach(println)
+groupByKey.repartition(1).saveAsTextFile("/user/cloudera/question48/groupByKey")
 
-//Step 4 : groupByKey #Generate key and value pairs (key is order status and value as an one 
-keyValue = allOrders.map(lambda line: (line.split(",")[3], 1)) 
-//#Using groupByKey, aggregate data based on status as a key 
-output = keyValue.groupByKey().map(lambda kv: (kv[0], sum(kv[1]))) 
-for line in output.collect(): print(line) 
+val reduceByKey = orders.reduceByKey( (v,c) => v + c)
+reduceByKey.collect.foreach(println)
+reduceByKey.repartition(1).saveAsTextFile("/user/cloudera/question48/reduceByKey")
 
-//Step 5 : reduceByKey #Generate key and value pairs (key is order status and value as an one 
-keyValue = allOrders.map(lambda line: (line.split(",")[3], 1)) 
-//#Using reduceByKey, aggregate data based on status as a key 
-output = keyValue.reduceByKey(lambda a, b: a + b) 
-for line in output.collect(): print(line) 
+val aggregateByKey = orders.aggregateByKey(0)( ( (z: Int,v: Int) =>  z + v) , ( (c: Int, v: Int) => c + v) )
+aggregateByKey.collect.foreach(println)
+aggregateByKey.repartition(1).saveAsTextFile("/user/cloudera/question48/aggregateByKey")
 
-//Step 6: aggregateByKey #Generate key and value pairs (key is order status and value as an one 
-keyValue = allOrders.map(lambda line: (line.split(",")[3], line))
-output = keyValue.aggregateByKey(0, lambda a, b: a + 1, lambda a, b: a + b) 
-for line in output.collect(): print(line) 
+val combineByKey = orders.combineByKey( ( (v: Int) => v) , ( (v: Int, c: Int) => v + c) , ( (v: Int, c: Int) => v + c))
+combineByKey.collect.foreach(println)
+combineByKey.repartition(1).saveAsTextFile("/user/cloudera/question48/combineByKey")
 
-//Step 7 : combineByKey #Generate key and value pairs (key is order status and vale as an one 
-keyValue = allOrders.map(lambda line: (line.split(",")[3], line)) 
-output = keyValue.combineByKey(lambda value: 1, lambda acc, value: acc + 1, lambda acc, value: acc + value) 
-for line in output.collect(): print(line) 
+$ hdfs dfs -ls /user/cloudera/question48/countByKey
+$ hdfs dfs -cat /user/cloudera/question48/countByKey/part-00000
 
-//Solution in Scala
-//Step 3: countByKey #Number of orders by status
-val allOrders = sc.textFile("p91_orders")
-val keyValue = allOrders.map(line => (line.split(",")(3), ""))
-val output = keyValue.countByKey().toList
-output.foreach(println)
+$ hdfs dfs -ls /user/cloudera/question48/groupByKey
+$ hdfs dfs -cat /user/cloudera/question48/groupByKey/part-00000
 
-//Step 4 : groupByKey #Generate key and value pairs (key is order status and value as an one 
-val keyValue = allOrders.map(line => (line.split(",")(3), 1))
-val output = keyValue.groupByKey().map(tuple => (tuple._1, tuple._2.size))
-output.collect().foreach(println)
+$ hdfs dfs -ls /user/cloudera/question48/reduceByKey
+$ hdfs dfs -cat /user/cloudera/question48/reduceByKey/part-00000
 
-//Step 5 : reduceByKey #Generate key and value pairs (key is order status and value as an one
-val keyValue = allOrders.map(line => (line.split(",")(3), 1))
-val output = keyValue.reduceByKey({case (v, v1) => v + v1})
-output.collect().foreach(println)
+$ hdfs dfs -ls /user/cloudera/question48/aggregateByKey
+$ hdfs dfs -cat /user/cloudera/question48/aggregateByKey/part-00000
 
-//Step 6: aggregateByKey #Generate key and value pairs (key is order status and value as an one 
-val keyValue = allOrders.map(line => (line.split(",")(3), line))
-val output = keyValue.aggregateByKey(0)((init,v) => init + 1, (v, v1) => v + v1)
-output.collect().foreach(println)
-
-//Step 7 : combineByKey #Generate key and value pairs (key is order status and vale as an one 
-val keyValue = allOrders.map(line => (line.split(",")(3), line))
-val output = keyValue.combineByKey(init => 1, (acc: Int, value) => acc + 1, (init: Int, acc: Int) => init + acce)
-output.collect().foreach(println)
+$ hdfs dfs -ls /user/cloudera/question48/combineByKey
+$ hdfs dfs -cat /user/cloudera/question48/combineByKey/part-00000
--- big-data-developer-with-apache-hadoop/training-cert-exercises-2/question49.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-2/question49.scala
@@ -1,72 +1,49 @@
-/**
- * Problem Scenario 88 : You have been given below three files
- * product.csv (Create this file in hdfs)
- * productID,productCode,name,quantity,price,supplierid
- * 1001,PEN,Pen Red,5000,1.23,501
- * 1002,PEN,Pen Blue,8000,1.25,501
- * 1003,PEN,Pen Black,2000,1.25,501
- * 1004,PEC,Pencil 2B,10000,0.48,502
- * 1005,PEC,Pencil 2H,8000,0.49,502
- * 1006,PEC,Pencil HB,0,9999.99,502
- * 2001,PEC,Pencil 3B,500,0.52,501
- * 2002,PEC,Pencil 4B,200,0.62,501
- * 2003,PEC,Pencil 5B,100,0.73,501
- * 2004,PEC,Pencil 6B,500,0.47,502
- * supplier.csv
- * supplierid,name,phone
- * 501,ABC Traders,88881111
- * 502,XYZ Company,88882222
- * 503,QQ Corp,88883333
- * products_suppliers.csv
- * productID,supplierID
- * 2001,501
- * 2002,501
- * 2003,501
- * 2004,502
- * 2001,503
- * Now accomplish all the queries given in solution.
- * 1. It is possible that, same product can be supplied by multiple supplier. Now find each product, its price according to each supplier.
- * 2. Find all the supllier name, who are supplying 'Pencil 3B'
- * 3. Find all the products , which are supplied by ABC Traders.
- */
+/** Question 49
+  * Problem Scenario 88 : You have been given below three files
+  * product.csv (Create this file in hdfs)
+  * productID,productCode,name,quantity,price,supplierid
+  * 1001,PEN,Pen Red,5000,1.23,501
+  * 1002,PEN,Pen Blue,8000,1.25,501
+  * 1003,PEN,Pen Black,2000,1.25,501
+  * 1004,PEC,Pencil 2B,10000,0.48,502
+  * 1005,PEC,Pencil 2H,8000,0.49,502
+  * 1006,PEC,Pencil HB,0,9999.99,502
+  * 2001,PEC,Pencil 3B,500,0.52,501
+  * 2002,PEC,Pencil 4B,200,0.62,501
+  * 2003,PEC,Pencil 5B,100,0.73,501
+  * 2004,PEC,Pencil 6B,500,0.47,502
+  * supplier.csv
+  * supplierid,name,phone
+  * 501,ABC Traders,88881111
+  * 502,XYZ Company,88882222
+  * 503,QQ Corp,88883333
+  * products_suppliers.csv
+  * productID,supplierID
+  * 2001,501
+  * 2002,501
+  * 2003,501
+  * 2004,502
+  * 2001,503
+  * 1001,501
+  * 1002,501
+  * 1003,501
+  * 1004,502
+  * 1005,502
+  * 1006,502
+  * Now accomplish all the queries given in solution.
+  * 1. It is possible that, same product can be supplied by multiple supplier. Now find each product, its price according to each supplier.
+  * 2. Find all the supllier name, who are supplying 'Pencil 3B'
+  * 3. Find all the products , which are supplied by ABC Traders.
+  */
+val l = List("productID")
+val product = sc.textFile("/user/cloudera/files/product.csv").map(line => line.split(",")).filter(r => !l.contains(r(0))).map(r => (r(0).toInt,r(1),r(2),r(3).toInt,r(4).toFloat,r(5).toInt)).toDF("pId","code","name","price","quantity","sId")
+val supplier = sc.textFile("/user/cloudera/files/supplier.csv").map(line => line.split(",")).map(r => (r(0).toInt,r(1),r(2))).toDF("supId","name","phone")
+val pr_sp = sc.textFile("/user/cloudera/files/products_suppliers.csv").map(line => line.split(",")).map(r => (r(0).toInt,r(1).toInt)).toDF("prId","sppId")
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+product.registerTempTable("pr")
+supplier.registerTempTable("sp")
+pr_sp.registerTempTable("pr_sp")
 
-//Explanation: Solution : 
-
-$ gedit product.csv supplier.csv products_suppliers.csv &
-$ cd /home/training/Desktop/files
-$ hdfs dfs -mkdir /files/product
-$ hdfs dfs -put product.csv supplier.csv products_suppliers.csv /files/product
-$ hdfs dfs -ls /files/product
-
-//Step 1 : It is possible that, same product can be supplied by multiple supplier. Now find each product, its price according to each supplier. 
-case class Product(id: Int, code: String, name: String, quantity: Int,price: Double, supplierId: Int)
-case class Supplier(id: Int, name: String, phone: String)
-case class ProductSupplier(productId: Int, supplierId: Int)
-
-val product = sc.textFile("/files/product/product.csv").map(line => line.split(",")).map(arr => new Product(arr(0).toInt,arr(1),arr(2),arr(3).toInt,arr(4).toDouble,arr(5).toInt)).toDF
-val supp = sc.textFile("/files/product/supplier.csv").map(line => line.split(",")).map(arr => new Supplier(arr(0).toInt,arr(1),arr(2))).toDF
-val proSupp = sc.textFile("/files/product/products_suppliers.csv").map(line => line.split(",")).map(arr => new ProductSupplier(arr(0).toInt,arr(1).toInt)).toDF
-
-product.registerTempTable("product")
-supp.registerTempTable("supplier")
-proSupp.registerTempTable("prod_supp")
-
-val results = sqlContext.sql("SELECT product.name AS ProductName, price, supplier.name AS SupplierName FROM prod_supp JOIN product ON (prod_supp.productId = product.id) JOIN supplier ON (prod_supp.supplierId = supplier.id)")
-results.registerTempTable("joined")
-results.show() 
-
-//Step 2 : Find all the supllier name, who are supplying 'Pencil 3B' 
-val step2 = sqlContext.sql("SELECT ProductName, SupplierName FROM joined WHERE ProductName = 'Pencil 3B'")
-step2.show()
-//Other solution
-val results = sqlContext.sql("SELECT p.name AS ProductName, s.name AS SupplierName FROM prod_supp AS ps JOIN product AS p ON ps.productId = p.id JOIN supplier AS s ON ps.supplierId = s.id WHERE p.name = 'Pencil 3B'") 
-results.show() 
-
-//Step 3 : Find all the products , which are supplied by ABC Traders. 
-val step3 = sqlContext.sql("SELECT ProductName, SupplierName FROM joined WHERE SupplierName = 'ABC Traders'")
-step3.show()
-//Other solution
-val results = sqlContext.sql("SELECT p.name AS ProductName, s.name AS SupplierName FROM product AS p, prod_supp AS ps, supplier AS s WHERE p.id = ps.productId AND ps.supplierId = s.id AND s.name = 'ABC Traders'") 
-results. show()
+sqlContext.sql("""SELECT pId,code,pr.name,price,quantity,sp.name FROM pr JOIN pr_sp ON (pId = prId) JOIN sp ON(sppId = supId)""").show()
+sqlContext.sql("""SELECT pr.name, sp.name FROM pr JOIN pr_sp ON (pId = prId) JOIN sp ON(sppId = supId) WHERE pr.name like("Pencil 3B")""").show()
+sqlContext.sql("""SELECT pr.*, sp.name FROM pr JOIN pr_sp ON (pId = prId) JOIN sp ON(sppId = supId) WHERE sp.name like("ABC Traders")""").show()
