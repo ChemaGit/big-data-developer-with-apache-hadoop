34c8a50a02e08943455372115f77996b231c18a8
Chema <josem32832@gmail.com>
2019-05-31 00:00:46 +0200

    30052019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-3/question61.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-3/question61.scala
@@ -1,76 +1,65 @@
 /** Question 61
- * Problem Scenario 24 : You have been given below comma separated employee information.
- * Data Set:
- * name,salary,sex,age
- * alok,100000,male,29
- * jatin,105000,male,32
- * yogesh,134000,male,39
- * ragini,112000,female,35
- * jyotsana,129000,female,39
- * valmiki,123000,male,29
- * Requirements:
- * Use the netcat service on port 44444, and nc above data line by line. Please do the following activities.
- * 1. Create a flume conf file using fastest channel, which write data in hive warehouse directory, in a table called flumemaleemployee (Create hive table as well tor given data).
- * 2. While importing, make sure only male employee data is stored. 
- */
+  * Problem Scenario 24 : You have been given below comma separated employee information.
+  * Data Set:
+  * name,salary,sex,age
+  * alok,100000,male,29
+  * jatin,105000,male,32
+  * yogesh,134000,male,39
+  * ragini,112000,female,35
+  * jyotsana,129000,female,39
+  * valmiki,123000,male,29
+  * Requirements:
+  * Use the netcat service on port 44444, and nc above data line by line. Please do the following activities.
+  * 1. Create a flume conf file using fastest channel, which write data in hive warehouse directory, in a table called flumemaleemployee (Create hive table as well tor given data).
+  * 2. While importing, make sure only male employee data is stored.
+  */
+$ hive
+  hive> use hadoopexam;
+hive> create table flumemaleemployee(name string, salary int, sex string, age int) row format delimited fields terminated by ',' location '/user/hive/warehouse/hadoopexam.db/flumemaleemployee';
+hive> show tables;
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+# example.conf: A single-node Flume configuration
 
-//Explanation: 
-//Step 1 : Create hive table for flumeemployee. 
-$ beeline -n training -p training -u jdbc:hive2://localhost:10000/default
-> CREATE TABLE flumemaleemployee ( name string, salary int, sex string, age int ) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','; 
+# Name the components on this agent
+  a1.sources = r1
+a1.sinks = k1
+a1.channels = c1
 
-//step 2 : Create flume configuration file, with below configuration for source, sink and channel and save it in flume5.conf. 
-#Define source , sink, channel and agent. 
-agent1.sources = source1 
-agent1.sinks = sink1 
-agent1.channels = channel1 
+# Describe/configure the source
+a1.sources.r1.type = netcat
+a1.sources.r1.bind = quickstart.cloudera
+a1.sources.r1.port = 44444
+a1.sources.r1.interceptors = i1
+a1.sources.r1.interceptors.i1.type = regex_filter
+a1.sources.r1.interceptors.i1.excludeEvents = true
+a1.sources.r1.interceptors.i1.regex = (female)
 
-# Describe/configure source1 
-agent1.sources.source1.type = netcat 
-agent1.sources.source1.bind = 127.0.0.1 
-agent1.sources.sourcel.port = 44444 
+# Describe the sink
+a1.sinks.k1.type = hdfs
+a1.sinks.k1.hdfs.path = /user/hive/warehouse/hadoopexam.db/flumemaleemployee
+a1.sinks.k1.hdfs.fileType = DataStream
+a1.sinks.k1.hdfs.hdfs.rollCount = 1
+a1.sinks.k1.hdfs.hdfs.batchSize = 1
 
-#Define interceptors 
-agent1.sources.source1.interceptors = i1 
-agent1.sources.source1.interceptors.i1.type = regex_filter 
-agent1.sources.source1.interceptors.i1.regex = female 
-agent1.sources.source1.interceptors.i1.excludeEvents = true 
+# Use a channel which buffers events in memory
+  a1.channels.c1.type = memory
+a1.channels.c1.capacity = 1000
+a1.channels.c1.transactionCapacity = 100
 
-## Describe sink1  
-agent1.sinks.sink1.type = hdfs 
-agent1.sinks.sink1.hdfs.path = /user/hive/warehouse/flume.db/flumemaleemployee 
-agent1.sinks.sink1.hdfs.writeFormat = Text 
-agent1.sinks.sink1.hdfs.fileType = DataStream 
+# Bind the source and sink to the channel
+  a1.sources.r1.channels = c1
+a1.sinks.k1.channel = c1
 
-# Now we need to define channel1 property. 
-agent1.channels.channel1.type = memory 
-agent1.channels.channel1.capacity = 1000 
-agent1.channels.channel1.transactionCapacity = 100 
+$ flume-ng agent --conf /home/cloudera/flume_demo --conf-file /home/cloudera/flume_demo/flumemaleemployee.conf --name a1 -Dflume.root.logger=INFO,console
 
-# Bind the source and sink to the channel 
-agent1.sources.source1.channels = channel1 
-agent1.sinks.sink1.channel = channel1 
+$ nc quickstart.cloudera 44444
 
-//step 3 : Run below command which will use this configuration file and append data in hdfs. Start flume service: 
-$ flume-ng agent --conf /home/training/flumeconf --conf-file /home/training/flumeconf/flume5.conf --name agent1 -Dflume.root.logger=INFO,console
+> alok,100000,male,29
+> jatin,105000,male,32
+> yogesh,134000,male,39
+> ragini,112000,female,35
+> jyotsana,129000,female,39
+> valmiki,123000,male,29
 
-//Step 4 : Open another terminal and use the netcat service, 
-$ nc localhost 44444 
-
-//Step 5 : Enter data line by line. 
-
-alok,100000,male,29 
-jatin,105000,male,32
-yogesh,134000,male,39 
-ragini,112000,female,35 
-jyotsana,129000,female,39 
-valmiki,123000,male,29 
-
-//Step 6 : Open hue and check the data is available in hive table or not. 
-select * from flumemaleemployee;
-//Step 7 : Stop flume service by pressing ctrl+c 
-$ ctrl+c
-//Step 8 : Calculate average salary on hive table using below query. You can use either hive command line tool or hue. 
-> select avg(salary) as avg_salary from flumemaleemployee;
+hive> select * from flumemaleemployee;
+hive> exit;
--- big-data-developer-with-apache-hadoop/training-cert-exercises-3/question62.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-3/question62.scala
@@ -1,119 +1,72 @@
 /** Question 62
- * Problem Scenario 27 : You need to implement near real time solutions for collecting information when submitted in file with below information.
- * Data
- * echo "IBM,100,20160104" >> /tmp/spooldir/bb/.bb.txt
- * echo "IBM,103,20160105" >> /tmp/spooldir/bb/.bb.txt
- * mv /tmp/spooldir/bb/.bb.txt /tmp/spooldir/bb/bb.txt
- * After few mins
- * echo "IBM,100.2,20160104" >> /tmp/spooldir/dr/.dr.txt
- * echo "IBM,103.1,20160105" >> /tmp/spooldir/dr/.dr.txt
- * mv /tmp/spooldir/dr/.dr.txt /tmp/spooldir/dr/dr.txt
- * Requirements:
- * You have been given below directory location (if not available than create it) /tmp/spooldir .
- * You have a finacial subscription for getting stock prices from BloomBerg as well as Reuters 
- * and using ftp you download every hour new files from their respective ftp site in directories /tmp/spooldir/bb and /tmp/spooldir/dr respectively.
- * As soon as file committed in this directory that needs to be available in hdfs in /tmp/flume/finance location in a single directory.
- * Write a flume configuration file named flume7.conf and use it to load data in hdfs with following additional properties .
- * 1. Spool /tmp/spooldir/bb and /tmp/spooldir/dr
- * 2. File prefix in hdfs should be events 
- * 3. File suffix should be .log
- * 4. If file is not commited and in use than it should have _ as prefix.
- * 5. Data should be written as text to hdfs
- */
-
-//Answer : See the explanation for Step by Step Solution and configuration.
-
-//Explanation: Solution : 
-//Step 1 : Create directory 
+  * Problem Scenario 27 : You need to implement near real time solutions for collecting information when submitted in file with below information.
+  * Data
+  * echo "IBM,100,20160104" >> /tmp/spooldir/bb/.bb.txt
+  * echo "IBM,103,20160105" >> /tmp/spooldir/bb/.bb.txt
+  * mv /tmp/spooldir/bb/.bb.txt /tmp/spooldir/bb/bb.txt
+  * After few mins
+  * echo "IBM,100.2,20160104" >> /tmp/spooldir/dr/.dr.txt
+  * echo "IBM,103.1,20160105" >> /tmp/spooldir/dr/.dr.txt
+  * mv /tmp/spooldir/dr/.dr.txt /tmp/spooldir/dr/dr.txt
+  * Requirements:
+  * You have been given below directory location (if not available than create it) /tmp/spooldir .
+  * You have a finacial subscription for getting stock prices from BloomBerg as well as Reuters
+  * and using ftp you download every hour new files from their respective ftp site in directories /tmp/spooldir/bb and /tmp/spooldir/dr respectively.
+  * As soon as file committed in this directory that needs to be available in hdfs in /user/cloudera/flume/finance location in a single directory.
+  * Write a flume configuration file named flume7.conf and use it to load data in hdfs with following additional properties .
+  * 1. Spool /tmp/spooldir/bb and /tmp/spooldir/dr
+  * 2. File prefix in hdfs should be events
+  * 3. File suffix should be .log
+  * 4. If file is not commited and in use than it should have _ as prefix.
+  * 5. Data should be written as text to hdfs
+  */
 $ mkdir /tmp/spooldir
-$ mkdir /tmp/spooldir/bb
-$ mkdir /tmp/spooldir/dr
+  $ mkdir /tmp/spooldir/bb
+  $ mkdir /tmp/spooldir/dr
 
-//Step 2: create hdfs directory
-$ hdfs dfs -mkdir /tmp/flume/finance
-
-//Step 3 : Create flume configuration file, with below configuration for 
-agent1.sources = source1 source2 
-agent1.sinks = sink1 
-agent1.channels = channel1 
-
-agent1.sources.source1.channels = channel1 
-agent1.sources.source2.channels = channel1 
-agent1.sinks.sink1.channel = channel1 
-
-agent1.sources.source1.type = spooldir 
-agent1.sources.source1.spoolDir = /tmp/spooldir/bb 
-
-agent1.sources.source2.type = spooldir 
-agent1.sources.source2.spoolDir = /tmp/spooldir/dr 
-
-agent1.sinks.sink1.type = hdfs 
-agent1.sinks.sink1.hdfs.path = /tmp/flume/finance 
-agent1.sinks.sink1.hdfs.filePrefix = events 
-agent1.sinks.sink1.hdfs.fileSuffix = .log 
-agent1.sinks.sink1.hdfs.inUsePrefix = _ 
-agent1.sinks.sink1.hdfs.fileType = DataStream 
-
-agent1.channels.channel1.type = file
-
-//Another possible solution for the agent 
+# example.conf: A single-node Flume configuration
 
 # Name the components on this agent
-a1.sources = r1 r2
-a1.sinks = k1 k2
-a1.channels = c1 c2
+  a1.sources = r1 r2
+  a1.sinks = k1
+a1.channels = c1
 
 # Describe/configure the source
 a1.sources.r1.type = spooldir
 a1.sources.r1.spoolDir = /tmp/spooldir/bb
-a1.sources.r1.fileHeader = false
 
 a1.sources.r2.type = spooldir
 a1.sources.r2.spoolDir = /tmp/spooldir/dr
-a1.sources.r2.fileHeader = false
-
 
 # Describe the sink
 a1.sinks.k1.type = hdfs
-a1.sinks.k1.hdfs.path = /tmp/flume/finance
-a1.sinks.k1.hdfs.filePrefix = events
+a1.sinks.k1.hdfs.path = /user/cloudera/flume/finance
+  a1.sinks.k1.hdfs.filePrefix = events
 a1.sinks.k1.hdfs.fileSuffix = .log
 a1.sinks.k1.hdfs.fileType = DataStream
-a1.sinks.k1.hdfs.inUseSuffix = -
-
-a1.sinks.k2.type = hdfs
-a1.sinks.k2.hdfs.path = /tmp/flume/finance
-a1.sinks.k2.hdfs.filePrefix = events
-a1.sinks.k2.hdfs.fileSuffix = .log
-a1.sinks.k2.hdfs.fileType = DataStream
-a1.sinks.k2.hdfs.inUseSuffix = -
+a1.sinks.k1.hdfs.inUsePrefix = _
 
 
 # Use a channel which buffers events in memory
-a1.channels.c1.type = memory
+  a1.channels.c1.type = memory
 a1.channels.c1.capacity = 1000
 a1.channels.c1.transactionCapacity = 100
 
-a1.channels.c2.type = memory
-a1.channels.c2.capacity = 1000
-a1.channels.c2.transactionCapacity = 100
+# a1.channels.c1.type = file
 
 # Bind the source and sink to the channel
-a1.sources.r1.channels = c1
-a1.sources.r2.channels = c2
+  a1.sources.r1.channels = c1
+a1.sources.r2.channels = c1
 a1.sinks.k1.channel = c1
-a1.sinks.k2.channel = c2
 
-//Step 4 : Run below command which will use this configuration file and append data in hdfs. 
-//Start flume service:  
-$ flume-ng agent --conf /home/training/flumeconf --conf-file /home/training/flumeconf/flume7.conf --name agent1 -Dflume.root.logger=INFO,console
+$ flume-ng agent --conf /home/cloudera/flume_demo --conf-file /home/cloudera/flume_demo/flume_finance.conf --name a1 -Dflume.root.logger=INFO,console
 
-//Step 5 : Open another terminal and create a file in /tmp/spooldir/ 
-$ echo "IBM,100,20160104" >> /tmp/spooldir/bb/.bb.txt 
-$ echo "IBM,103,20160105" >> /tmp/spooldir/bb/.bb.txt 
-$ mv /tmp/spooldir/bb/.bb.txt /tmp/spooldir/bb/bb.txt 
-
-//After few mins 
-$ echo "IBM,100.2,20160104" >> /tmp/spooldir/dr/.dr.txt 
-$ echo "IBM,103.1,20160105" >> /tmp/spooldir/dr/.dr.txt 
+$ echo "IBM,100,20160104" >> /tmp/spooldir/bb/.bb.txt
+$ echo "IBM,103,20160105" >> /tmp/spooldir/bb/.bb.txt
+$ mv /tmp/spooldir/bb/.bb.txt /tmp/spooldir/bb/bb.txt
+// After few mins
+$ echo "IBM,100.2,20160104" >> /tmp/spooldir/dr/.dr.txt
+$ echo "IBM,103.1,20160105" >> /tmp/spooldir/dr/.dr.txt
 $ mv /tmp/spooldir/dr/.dr.txt /tmp/spooldir/dr/dr.txt
+
+$ hdfs dfs -cat /user/cloudera/flume/finance/*.log
