6f13887ee9a34a2ab79e6f89d7c48c84357ac7ab
Chema <josem32832@gmail.com>
2019-06-04 23:29:16 +0200

    04062019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question74.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question74.scala
@@ -1,55 +1,31 @@
 /** Question 74
- * Problem Scenario 45 : You have been given 2 files , with the content as given Below
- * (spark12/technology.txt)
- * (spark12/salary.txt)
- * (spark12/technology.txt)
- * first,last,technology
- * Amit,Jain,java
- * Lokesh,kumar,unix
- * Mithun,kale,spark
- * Rajni,vekat,hadoop
- * Rahul,Yadav,scala
- * (spark12/salary.txt)
- * first,last,salary
- * Amit,Jain,100000
- * Lokesh,kumar,95000
- * Mithun,kale,150000
- * Rajni,vekat,154000
- * Rahul,Yadav,120000
- * Write a Spark program, which will join the data based on first and last name and save the joined results in following format, first Last.technology.salary
- */
+  * Problem Scenario 45 : You have been given 2 files , with the content as given Below
+  * (technology.txt)
+  * (salary.txt)
+  * (technology.txt)
+  * first,last,technology
+  * Amit,Jain,java
+  * Lokesh,kumar,unix
+  * Mithun,kale,spark
+  * Rajni,vekat,hadoop
+  * Rahul,Yadav,scala
+  * (salary.txt)
+  * first,last,salary
+  * Amit,Jain,100000
+  * Lokesh,kumar,95000
+  * Mithun,kale,150000
+  * Rajni,vekat,154000
+  * Rahul,Yadav,120000
+  * Write a Spark program, which will join the data based on first and last name and save the joined results in following format, first Last.technology.salary
+  */
+$ gedit /user/cloudera/files/technology.txt
+$ gedit /user/cloudera/files/salary.txt
+$ hdfs dfs -put /user/cloudera/files/technology.txt /user/cloudera/files
+$ hdfs dfs -put /user/cloudera/files/salary.txt /user/cloudera/files
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+val tech = sc.textFile("/user/cloudera/files/technology.txt").map(line => line.split(",")).map(r => ( (r(0),r(1)),r(2) ))
+val  salary = sc.textFile("/user/cloudera/files/salary.txt").map(line => line.split(",")).map(r => ( (r(0),r(1)),r(2) ))
+val joined = tech.join(salary).map({case( ( (f,l),(t,s)) ) => "%s,%s,%s,%s".format(f,l,t,s)})
+joined.repartition(1).saveAsTextFile("/user/cloudera/question74/result")
 
-//Explanation: Solution : 
-//Step 1: create the files locally and upload the files from local system to hdfs
-$ gedit salary.txt technology.txt &
-$ hdfs dfs -put salary.txt technology.txt /files
-$ hdfs dfs -cat /files/salary.txt
-$ hdfs dfs -cat /files/technology.txt
-
-//Step 2 : Load all file as an RDD 
-val technology = sc.textFile("/files/technology.txt").map(e => e.split(",")) 
-val salary = sc.textFile("/files/salary.txt").map(e => e.split(",")) 
-
-//Step 3 : Now create Key.value pair of data and join them. 
-val joined = technology.map(e=>((e(0),e(1)),e(2))).join(salary.map(e=>((e(0),e(1)),e(2)))) 
-
-//Step 4 : Save the results in a text file as below. 
-joined.repartition(1).saveAsTextFile("/spark12")
-
-/******ANOTHER POSSIBLE SOLUTION*********/
-//Step 1: create the files locally and upload the files from local system to hdfs
-$ gedit salary.txt technology.txt &
-$ hdfs dfs -put salary.txt technology.txt /files
-$ hdfs dfs -cat /files/salary.txt
-$ hdfs dfs -cat /files/technology.txt
-
-//Step 2: Write a Spark program, which will join the data based on first and last name and save the joined results in following format: first,Last,technology,salary
-val rddSalary = sc.textFile("/files/salary.txt").map(line => line.split(",")).map(arr => ((arr(0),arr(1)),arr(2)))
-val rddTech = sc.textFile("/files/technology.txt").map(line => line.split(",")).map(arr => ((arr(0),arr(1)),arr(2)))
-val joined = rddTech.join(rddSalary)
-val format = joined.map({case(((f,l),(t,s))) => "%s,%s,%s,%s".format(f,l,t,s)})
-format.repartition(1).saveAsTextFile("/files/salaryTech")
-
-//spark-shell -i probe.scala
+$ hdfs dfs -cat /user/cloudera/question74/result/part*
--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question75.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question75.scala
@@ -1,29 +1,16 @@
 /** Question 75
- * Problem Scenario 54 : You have been given below code snippet.
- * val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"))
- * val b = a.map(x => (x.length, x))
- * operation1
- * Write a correct code snippet for operation1 which will produce desired output, shown below.
- * Array[(Int, String)] = Array((4,lion), (7,panther), (3,dogcat), (5,tigereagle))
- */
-
-//Answer : See the explanation for Step by Step Solution and configuration.
-
-//Explanation: Solution : 
-b.foldByKey("")(_ + _).collect 
-//foldByKey [Pair] Very similar to fold, but performs the folding separately for each key of the RDD. 
-//This function is only available if the RDD consists of two-component tuples Listing Variants 
-//def foldByKey(zeroValue: V)(func: (V, V) => V): RDD[(K, V)] 
-//def foldByKey(zeroValue: V, numPartitions: Int)(func: (V, V) => V): RDD[(K, V)] 
-//def foldByKey(zeroValue: V, partitioner: Partitioner)(func: (V, V) => V): RDD[(K, V)]
-
+  * Problem Scenario 54 : You have been given below code snippet.
+  * val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"))
+  * val b = a.map(x => (x.length, x))
+  * operation1
+  * Write a correct code snippet for operation1 which will produce desired output, shown below.
+  * Array[(Int, String)] = Array((4,lion), (7,panther), (3,dogcat), (5,tigereagle))
+  */
 val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"))
 val b = a.map(x => (x.length, x))
-b.foldByKey("")(_ + _).collect
-res2: Array[(Int, String)] = Array((4,lion), (5,tigereagle), (3,dogcat), (7,panther))
+b.reduceByKey( (v, c) => v + c).collect
+// res0: Array[(Int, String)] = Array((4,lion), (5,tigereagle), (3,dogcat), (7,panther))
 
-/*OTHER POSSIBLE SOLUTION WOULD BE...**/
-val a = sc.parallelize(List("dog", "tiger", "lion", "cat", "panther", "eagle"))
-val b = a.map(x => (x.length, x))
-b.reduceByKey(_ + _).collect
-res1: Array[(Int, String)] = Array((4,lion), (5,tigereagle), (3,dogcat), (7,panther))
+// another solution could be
+b.foldByKey("")( (v,c) => v + c).collect
+//res1: Array[(Int, String)] = Array((4,lion), (5,tigereagle), (3,dogcat), (7,panther))
