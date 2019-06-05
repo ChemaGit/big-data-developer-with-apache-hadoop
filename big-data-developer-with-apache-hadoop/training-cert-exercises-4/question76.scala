5a6d55f2c958a53ca87984e6f5f118cb96380ffd
Chema <josem32832@gmail.com>
2019-06-05 22:41:56 +0200

    05062019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question76.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question76.scala
@@ -1,17 +1,14 @@
 /** Question 76
- * Problem Scenario 38 : You have been given an RDD as below,
- * val rdd: RDD[Array[Byte]]
- * Now you have to save this RDD as a SequenceFile. And below is the code snippet.
- * import org.apache.hadoop.io.compress.GzipCodec
- * rdd.map(bytesArray => (A.get(), new B(bytesArray))).saveAsSequenceFile("/output/path",classOut[GzipCodec])
- * What would be the correct replacement for A and B in above snippet.
- */
+  * Problem Scenario 38 : You have been given an RDD as below,
+  * val rdd = sc.parallelize(Array[Array[Byte]]())
+  * Now you have to save this RDD as a SequenceFile. And below is the code snippet.
+  * import org.apache.hadoop.io.compress.BZip2Codec
+  * rdd.map(bytesArray => (A.get(), new B(bytesArray))).saveAsSequenceFile("/user/cloudera/question76/sequence",Some(classOf[BZip2Codec]))
+  * What would be the correct replacement for A and B in above snippet.
+  */
+val rdd = sc.parallelize(Array[Array[Byte]]())
+import org.apache.hadoop.io.compress.BZip2Codec
+rdd.map(bytesArray => (org.apache.hadoop.io.NullWritable.get(), new org.apache.hadoop.io.BytesWritable(bytesArray))).saveAsSequenceFile("/user/cloudera/question76/sequence",Some(classOf[BZip2Codec]))
 
-Answer : See the explanation for Step by Step Solution and configuration.
-
-Explanation: Solution : A. NullWritable B. BytesWritable
-
-val rdd: RDD[Array[Byte]]
-import org.apache.hadoop.io.compress.GzipCodec
-
-rdd.map(bytesArray => (NullWritable.get(), new BytesWritable(bytesArray))).saveAsSequenceFile("/output/path",classOut[GzipCodec])
+$ hdfs dfs -ls /user/cloudera/question76/sequence
+$ hdfs dfs -text /user/cloudera/question76/sequence/*
--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question77.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question77.scala
@@ -1,34 +1,28 @@
 /** Question 77
- * Problem Scenario 51 : You have been given below code snippet.
- * val a = sc.parallelize(List(1, 2,1, 3), 1)
- * val b = a.map((_, "b"))
- * val c = a.map((_, "c"))
- * Operation_xyz
- * Write a correct code snippet for Operation_xyz which will produce below output.
- * Output:
- * Array[(Int, (Iterable[String], Iterable[String]))] = Array((2,(ArrayBuffer(b),ArrayBuffer(c))),(3,(ArrayBuffer(b),ArrayBuffer(c))),(1,(ArrayBuffer(b, b),ArrayBuffer(c, c)))
- */
-
-//Answer : See the explanation for Step by Step Solution and configuration.
-
-//Explanation: Solution : 
-b.cogroup(c).collect 
-//cogroup [Pair], groupWith [Pair] A very powerful set of functions that allow grouping up to 3 key-value RDDs together using their keys. 
-//Another example 
-val x = sc.parallelize(List((1, "apple"), (2, "banana"), (3, "orange"), (4, "kiwi")), 2) 
-val y = sc.parallelize(List((5, "computer"), (1, "laptop"), (1, "desktop"), (4, "iPad")), 2) 
-x.cogroup(y).collect 
-Array[(Int, (Iterable[String], Iterable[String]))] = Array( (4,(ArrayBuffer(kiwi),ArrayBuffer(iPad))), (2,(ArrayBuffer(banana),ArrayBuffer())), (3,(ArrayBuffer(orange),ArrayBuffer())), (1 ,(ArrayBuffer(apple),ArrayBuffer(laptop, desktop))), (5,(ArrayBuffer(),ArrayBuffer(computer))))
-
+  * Problem Scenario 51 : You have been given below code snippet.
+  * val a = sc.parallelize(List(1, 2,1, 3), 1)
+  * val b = a.map((_, "b"))
+  * val c = a.map((_, "c"))
+  * Operation_xyz
+  * Write a correct code snippet for Operation_xyz which will produce below output.
+  * Output:
+  * Array[(Int, (Iterable[String], Iterable[String]))] = Array(
+  * (2,(ArrayBuffer(b),ArrayBuffer(c))),
+  * (3,(ArrayBuffer(b),ArrayBuffer(c))),
+  * (1,(ArrayBuffer(b, b),ArrayBuffer(c, c)))
+  */
 val a = sc.parallelize(List(1, 2,1, 3), 1)
 val b = a.map((_, "b"))
 val c = a.map((_, "c"))
+//cogroup [Pair], groupWith [Pair] A very powerful set of functions that allow grouping up to 3 key-value RDDs together using their keys.
 b.cogroup(c).collect
-Array[(Int, (Iterable[String], Iterable[String]))] = Array((1,(CompactBuffer(b, b),CompactBuffer(c, c))), (3,(CompactBuffer(b),CompactBuffer(c))), (2,(CompactBuffer(b),CompactBuffer(c))))
 
-/**ANOTHER POSSIBLE SOLUTION**/
-val a = sc.parallelize(List(1, 2,1, 3), 1)
-val b = a.map((_, "b"))
-val c = a.map((_, "c"))
+// Alternative solution
 b.groupByKey().join(c.groupByKey()).collect
-Array[(Int, (Iterable[String], Iterable[String]))] = Array((1,(CompactBuffer(b, b),CompactBuffer(c, c))), (3,(CompactBuffer(b),CompactBuffer(c))), (2,(CompactBuffer(b),CompactBuffer(c))))
+
+// Another example
+val x = sc.parallelize(List((1, "apple"), (2, "banana"), (3, "orange"), (4, "kiwi")), 2)
+val y = sc.parallelize(List((5, "computer"), (1, "laptop"), (1, "desktop"), (4, "iPad")), 2)
+x.cogroup(y).collect
+// Array[(Int, (Iterable[String], Iterable[String]))] = Array( (4,(ArrayBuffer(kiwi),ArrayBuffer(iPad))), (2,(ArrayBuffer(banana),ArrayBuffer())),
+// (3,(ArrayBuffer(orange),ArrayBuffer())), (1 ,(ArrayBuffer(apple),ArrayBuffer(laptop, desktop))), (5,(ArrayBuffer(),ArrayBuffer(computer))))
