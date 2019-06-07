3c8c264bdd45ac1e4ecf3fc7b2b9c270c1e15ae7
Chema <josem32832@gmail.com>
2019-06-07 22:57:38 +0200

    07062019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question85.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question85.scala
@@ -1,88 +1,70 @@
 /** Question 85
- * Problem Scenario 14 : You have been given following mysql database details as well as other info.
- * user=retail_dba
- * password=cloudera
- * database=retail_db
- * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
- * Please accomplish following activities.
- * 1. Create a csv file named updated_departments.csv with the following contents in local file system.
- * updated_departments.csv
- * 2,fitness
- * 3,footwear
- * 12,fathematics
- * 13,fcience
- * 14,engineering
- * 1000,management
- * 2. Upload this csv file to hdfs filesystem,
- * 3. Now export this data from hdfs to mysql retaildb.departments table. During upload make sure existing department will just updated and new departments needs to be inserted.
- * 4. Now update updated_departments.csv file with below content.
- * 2,Fitness
- * 3,Footwear
- * 12,Fathematics
- * 13,Science
- * 14,Engineering
- * 1000,Management
- * 2000,Quality Check
- * 5. Now upload this file to hdfs.
- * 6. Now export this data from hdfs to mysql retail_db.departments table. During upload make sure existing department will just updated and no new departments needs to be inserted.
- */
+  * Problem Scenario 14 : You have been given following mysql database details as well as other info.
+  * user=retail_dba
+  * password=cloudera
+  * database=retail_db
+  * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
+  * Please accomplish following activities.
+  * 1. Create a csv file named updated_departments.csv with the following contents in local file system.
+  * updated_departments.csv
+  * 2,fitness
+  * 3,footwear
+  * 12,fathematics
+  * 13,fcience
+  * 14,engineering
+  * 1000,management
+  * 2. Upload this csv file to hdfs filesystem,
+  * 3. Now export this data from hdfs to mysql retaildb.departments table. During upload make sure existing department will just updated and new departments needs to be inserted.
+  * 4. Now update updated_departments.csv file with below content.
+  * 2,Fitness
+  * 3,Footwear
+  * 12,Fathematics
+  * 13,Science
+  * 14,Engineering
+  * 1000,Management
+  * 2000,Quality Check
+  * 5. Now upload this file to hdfs.
+  * 6. Now export this data from hdfs to mysql retail_db.departments table. During upload make sure existing department will just updated and no new departments needs to be inserted.
+  */
+$ gedit /home/cloudera/files/updated_departments.csv &
+  $ hdfs dfs -mkdir /user/cloudera/question85/
+  $ hdfs dfs -mkdir /user/cloudera/question85/updated_departments
+$ hdfs dfs -put /home/cloudera/files/updated_departments.csv /user/cloudera/question85/updated_departments
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+sqoop export \
+--connect jdbc:mysql://quickstart:3306/retail_db \
+  --username root \
+  --password cloudera \
+  --table departments \
+  --export-dir /user/cloudera/question85/updated_departments \
+  --update-key "department_id" \
+  --update-mode allowinsert \
+--input-fields-terminated-by ',' \
+  --input-lines-terminated-by '\n' \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
 
-//Explanation: Solution : 
-//Step 1 : Create a csv tile named updated_departments.csv with give content.
-$ gedit updated_departments.csv & 
-
-//Step 2 : Now upload this tile to HDFS. Create a directory called newdata. 
-$ hdfs dfs -mkdir new_data 
-$ hdfs dfs -put updated_departments.csv newdata/ 
-
-//Step 3 : Check whether tile is uploaded or not. 
-$ hdfs dfs -ls new_data
-$ hdfs dfs -cat newdata/updated_departments.csv 
-
-//Step 4 : Export this file to departments table using sqoop. 
-$ sqoop export \
---connect jdbc:mysql://quickstart:3306/retail_db \ 
---username retail_dba \ 
---password cloudera \ 
---table departments \ 
---export-dir new_data \ 
---batch \ 
---num-mappers 1 \ 
---update-key department_id \ 
---update-mode allowinsert 
-
-//Step 5 : Check whether required data upsert is done or not. 
-$ mysql -u retail_dba -p cloudera 
-mysql> show databases; 
-mysql> use retail db; 
-mysql> show tables; 
+$ mysql -u root -p
+mysql> use retail_db;
 mysql> select * from departments;
 
-//Step 6 : Update updated_departments.csv file. 
-$ gedit updated_departments.csv &
+$ gedit /home/cloudera/files/updated_departments.csv &
+  $ hdfs dfs -put -f /home/cloudera/files/updated_departments.csv /user/cloudera/question85/updated_departments
 
-//Step 7 : Override the existing file in hdfs.
-$ hdfs dfs -rm newdata/updated_departments.csv  
-$ hdfs dfs -put updated_departments.csv newdata/ 
-$ hdfs dfs -cat newdata/updated_departments.csv
+sqoop export \
+--connect jdbc:mysql://quickstart:3306/retail_db \
+  --username root \
+  --password cloudera \
+  --table departments \
+  --export-dir /user/cloudera/question85/updated_departments \
+  --update-key "department_id" \
+  --update-mode updateonly \
+--input-fields-terminated-by ',' \
+  --input-lines-terminated-by '\n' \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
 
-//Step 8 : Now do the Sqoop export as per the requirement. 
-$ sqoop export \
---connect jdbc:mysql://quickstart:3306/retail_db \ 
---username retail_dba \ 
---password cloudera \ 
---table departments \ 
---export-dir new_data \ 
---batch \ 
---num-mappers 1 \ 
---update-key department_id \ 
---update-mode updateonly 
-
-//Step 9 : Check whether required data update is done or not. 
-$ mysql -u retail_dba -p cloudera 
-mysql> show databases; 
-mysql> use retail db; 
-mysql> show tables; 
 mysql> select * from departments;
+mysql> exit;
--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question86.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question86.scala
@@ -1,79 +1,53 @@
 /** Question 86
- * Problem Scenario 44 : You have been given 4 files , with the content as given below:
- * spark11/file_1.txt
- * Apache Hadoop is an open-source software framework written in Java for distributed
- * storage and distributed processing of very large data sets on computer clusters built from
- * commodity hardware. All the modules in Hadoop are designed with a fundamental
- * assumption that hardware failures are common and should be automatically handled by the framework
- * spark11/file_2.txt
- * The core of Apache Hadoop consists of a storage part known as Hadoop Distributed File
- * System (HDFS) and a processing part called MapReduce. Hadoop splits files into large
- * blocks and distributes them across nodes in a cluster. To process data, Hadoop transfers
- * packaged code for nodes to process in parallel based on the data that needs to be processed.
- * spark11/file_3.txt
- * his approach takes advantage of data locality nodes manipulating the data they have
- * access to to allow the dataset to be processed faster and more efficiently than it would be
- * in a more conventional supercomputer architecture that relies on a parallel file system
- * where computation and data are distributed via high-speed networking
- * spark11/file_4.txt
- * Apache Storm is focused on stream processing or what some call complex event
- * processing. Storm implements a fault tolerant method for performing a computation or
- * pipelining multiple computations on an event as it flows into a system. One might use
- * Storm to transform unstructured data as it flows into a system into a desired format
- * (spark11/file_1.txt)
- * (spark11/file_2.txt)
- * (spark11/file_3.txt)
- * (spark11/file_4.txt)
- * Write a Spark program, which will give you the highest occurring words in each file. With their file name and highest occurring words.
- */
+  * Problem Scenario 44 : You have been given 4 files , with the content as given below:
+  * spark11/file_1.txt
+  * Apache Hadoop is an open-source software framework written in Java for distributed
+  * storage and distributed processing of very large data sets on computer clusters built from
+  * commodity hardware. All the modules in Hadoop are designed with a fundamental
+  * assumption that hardware failures are common and should be automatically handled by the framework
+  * spark11/file_2.txt
+  * The core of Apache Hadoop consists of a storage part known as Hadoop Distributed File
+  * System (HDFS) and a processing part called MapReduce. Hadoop splits files into large
+  * blocks and distributes them across nodes in a cluster. To process data, Hadoop transfers
+  * packaged code for nodes to process in parallel based on the data that needs to be processed.
+  * spark11/file_3.txt
+  * his approach takes advantage of data locality nodes manipulating the data they have
+  * access to to allow the dataset to be processed faster and more efficiently than it would be
+  * in a more conventional supercomputer architecture that relies on a parallel file system
+  * where computation and data are distributed via high-speed networking
+  * spark11/file_4.txt
+  * Apache Storm is focused on stream processing or what some call complex event
+  * processing. Storm implements a fault tolerant method for performing a computation or
+  * pipelining multiple computations on an event as it flows into a system. One might use
+  * Storm to transform unstructured data as it flows into a system into a desired format
+  * (spark11/file_1.txt)
+  * (spark11/file_2.txt)
+  * (spark11/file_3.txt)
+  * (spark11/file_4.txt)
+  * Write a Spark program, which will give you the highest occurring words in each file. With their file name and highest occurring words.
+  */
+$ gedit /home/cloudera/files/file_1.txt &
+  $ gedit /home/cloudera/files/file_2.txt &
+  $ gedit /home/cloudera/files/file_3.txt &
+  $ gedit /home/cloudera/files/file_4.txt &
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+$ hdfs dfs -put /home/cloudera/files/file_1.txt /user/cloudera/files
+$ hdfs dfs -put /home/cloudera/files/file_2.txt /user/cloudera/files
+$ hdfs dfs -put /home/cloudera/files/file_3.txt /user/cloudera/files
+$ hdfs dfs -put /home/cloudera/files/file_4.txt /user/cloudera/files
 
-//Explanation: Solution : 
-//Step 1 : Create all 4 file first using Hue in hdfs. 
-$ gedit file_1.txt file_2.txt file_3.txt file_4.txt &
-$ hdfs dfs -mkdir /files/spark11/
-$ hdfs dfs -put file_1.txt file_2.txt file_3.txt file_4.txt /files/spark11/
-$ hdfs dfs -ls /files/spark11/
-$ hdfs dfs -cat /files/spark11/file_4.txt
+val n1 = "file_1.txt"
+val n2 = "file_2.txt"
+val n3 = "file_3.txt"
+val n4 = "file_4.txt"
+val filt = List(""," ")
 
-//Step 2 : Load all file as an RDD 
-val file1 = sc.textFile("/files/spark11/file_1.txt") 
-val file2 = sc.textFile("/files/spark11/file_2.txt") 
-val file3 = sc.textFile("/files/spark11/file_3.txt") 
-val file4 = sc.textFile("/files/spark11/file_4.txt") 
+val f1 = List( (n1,sc.textFile("/user/cloudera/files/file_1.txt").flatMap(line => line.split("\\W")).filter(w => !filt.contains(w)).map(w => (w,1)).reduceByKey( (v,c) => v + c).sortBy(t => t._2,false).first) )
+val f2 = List( (n2,sc.textFile("/user/cloudera/files/file_2.txt").flatMap(line => line.split("\\W")).filter(w => !filt.contains(w)).map(w => (w,1)).reduceByKey( (v,c) => v + c).sortBy(t => t._2,false).first) )
+val f3 = List( (n3,sc.textFile("/user/cloudera/files/file_3.txt").flatMap(line => line.split("\\W")).filter(w => !filt.contains(w)).map(w => (w,1)).reduceByKey( (v,c) => v + c).sortBy(t => t._2,false).first) )
+val f4 = List( (n4,sc.textFile("/user/cloudera/files/file_4.txt").flatMap(line => line.split("\\W")).filter(w => !filt.contains(w)).map(w => (w,1)).reduceByKey( (v,c) => v + c).sortBy(t => t._2,false).first) )
 
-//Step 3 : Now do the word count for each file and sort in reverse order of count. 
-val content1 = file1.flatMap( line => line.split(" ")).map(word => (word,1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).map(e=>e.swap).collect 
-val content2 = file2.flatMap( line => line.split(" ")).map(word => (word,1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).map(e=>e.swap).collect  
-val content3 = file3.flatMap( line => line.split(" ")).map(word => (word,1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).map(e=>e.swap).collect  
-val content4 = file4.flatMap( line => line.split(" ")).map(word => (word,1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).map(e=>e.swap).collect  
+val result = sc.parallelize(f1.union(f2).union(f3).union(f4)).map({case( (f,(w,c)) ) => "file: %s ==> word: %s ==> count: %d".format(f,w,c)})
+result.repartition(1).saveAsTextFile("/user/cloudera/question86")
 
-//Step 4 : Split the data and create RDD of all Employee objects. 
-val file1word = sc.makeRDD(Array(file1.name+"->"+content1(0)._1+"-"+content1(0)._2)) 
-val file2word = sc.makeRDD(Array(file2.name+"->"+content2(0)._1+"-"+content2(0)._2)) 
-val file3word = sc.makeRDD(Array(file3.name+"->"+content3(0)._1+"-"+content3(0)._2)) 
-val file4word = sc.makeRDD(Array(file4.name+"->"+content4(0)._1+"-"+content4(0)._2)) 
-
-//Step 5: Union all the RDDS 
-val unionRDDs = file1word.union(file2word).union(file3word).union(file4word) 
-
-//Step 6 : Save the results in a text file as below. 
-unionRDDs.repartition(1).saveAsTextFile("/files/spark11/union")
-
-/****ANOTHER SOLUTION***********/
-//Step 1: Create the files and upload them to HDFS
-$ gedit file_1.txt file_2.txt file_3.txt file_4.txt &
-$ hdfs dfs -mkdir /files/spark11/
-$ hdfs dfs -put file_1.txt file_2.txt file_3.txt file_4.txt /files/spark11/
-$ hdfs dfs -ls /files/spark11/
-$ hdfs dfs -cat /files/spark11/file_4.txt
-
-//Step 2: Write a Spark program, which will give you the highest occurring words in each file. With their file name and highest occurring words.
-val files = sc.wholeTextFiles("/files/spark11/*")
-val arrs = files.map({case(k, v) => (k, v.split("\\W").filter(w => !w.isEmpty)) })
-var max = 0
-val grouped = arrs.mapValues(v => v.groupBy(t => t).values.map({case(arr) => {if(arr.length > max) max = arr.length; arr}}).filter(arr => arr.length >= max))
-val sorted = grouped.mapValues(v => v.toList.map(arr => (arr(0), arr.length)).sortBy(t => t._1.toLowerCase) ).mapValues({case(l) => if(!l.isEmpty) l.head})
-sorted.collect.foreach({case( (n,(w,o)) ) => println("file: %s, word: %s, occurrences: %d".format(n,w,o))})
-sorted.repartition(1).saveAsTextFile("/files/spark11/result")
+$ hdfs dfs -cat /user/cloudera/question86/part*
