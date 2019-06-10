4198ab60843c44a531a7d6f5d827c0d2a812b95b
Chema <josem32832@gmail.com>
2019-06-10 15:23:16 +0200

    10062019

--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question88.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question88.scala
@@ -1,59 +1,52 @@
 /** Question 88
- * Problem Scenario 11 : You have been given following mysql database details as well as other info.
- * user=retail_dba
- * password=cloudera
- * database=retail_db
- * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
- * Please accomplish following.
- * 1. Import departments table in a directory called departments.
- * 2. Once import is done, please insert following 5 records in departments mysql table.
- * mysql> Insert into departments values(16, 'Physics');
- * mysql> Insert into departments values(17, 'Chemistry');
- * mysql> Insert into departments values(18, 'Superb Maths');
- * mysql> Insert into departments values(19, 'Superb Science');
- * mysql> Insert into departments values(20, 'Computer Engineering');
- * 3. Now import only new inserted records and append to existring directory . which has been created in first step.
- */
+  * Problem Scenario 11 : You have been given following mysql database details as well as other info.
+  * user=retail_dba
+  * password=cloudera
+  * database=retail_db
+  * jdbc URL = jdbc:mysql://quickstart:3306/retail_db
+  * Please accomplish following.
+  * 1. Import departments table in a directory called /user/cloudera/question88/departments.
+  * 2. Once import is done, please insert following 5 records in departments mysql table.
+  * mysql> Insert into departments values(16, 'Physics');
+  * mysql> Insert into departments values(17, 'Chemistry');
+  * mysql> Insert into departments values(18, 'Superb Maths');
+  * mysql> Insert into departments values(19, 'Superb Science');
+  * mysql> Insert into departments values(20, 'Computer Engineering');
+  * 3. Now import only new inserted records and append to existring directory . which has been created in first step.
+  */
+sqoop import \
+--connect jdbc:mysql://quickstart:3306/retail_db \
+  --username root \
+  --password cloudera \
+  --table departments \
+  --delete-target-dir \
+  --target-dir /user/cloudera/question88/departments \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
 
-//Answer : See the explanation for Step by Step Solution and configuration.
+$ hdfs dfs -cat /user/cloudera/question88/departments/part*
 
-//Explanation: Solution : 
-//Step 1 : Clean already imported data. (In real exam, please make sure you dont delete data generated from previous exercise). 
-$ hadoop fs -rm -R departments 
+$ mysql -u root -p
+mysql> use retail_db;
+mysql> Insert into departments values(16, 'Physics');
+mysql> Insert into departments values(17, 'Chemistry');
+mysql> Insert into departments values(18, 'Superb Maths');
+mysql> Insert into departments values(19, 'Superb Science');
+mysql> Insert into departments values(20, 'Computer Engineering');
+mysql> exit;
 
-//Step 2 : Import data in departments directory. 
-$ sqoop import \ 
---connect jdbc:mysql://quickstart:3306/retail_db \ 
---username retail_dba \ 
---password cloudera \ 
---table departments \ 
---target-dir /user/cloudera/departments 
+sqoop import \
+--connect jdbc:mysql://quickstart:3306/retail_db \
+  --username root \
+  --password cloudera \
+  --table departments \
+  --incremental append \
+  --check-column "department_id" \
+  --last-value 10005 \
+  --target-dir /user/cloudera/question88/departments \
+  --outdir /home/cloudera/outdir \
+--bindir /home/cloudera/bindir \
+--num-mappers 1
 
-//Step 3 : Insert the five records in departments table. 
-$ mysql -user=retail_dba --password=cloudera 
-mysql> use retail_db; 
-mysql> Insert into departments values(10, "physics"); 
-mysql> Insert into departments values(11, "Chemistry"); 
-mysql> Insert into departments values(12, "Maths"); 
-mysql> Insert into departments values(13, "Science"); 
-mysql> Insert into departments values(14, "Engineering"); 
-mysql> commit; 
-mysql> select * from departments; 
-
-//Step 4 : Get the maximum value of departments from last import, that should be 7
-hdfs dfs -cat /user/cloudera/departments/part* that should be 7 
-
-//Step 5 : Do the incremental import based on last import and append the results. 
-sqoop import \ 
---connect "jdbc:mysql://quickstart.cloudera:330G/retail_db" \ 
---username retail_dba \ 
---password cloudera \ 
---table departments \ 
---target-dir /user/cloudera/departments \ 
---append \ 
---check-column "department_id" \ 
---incremental append \ 
---last-value 7 
-
-//Step 6 : Now check the result. 
-hdfs dfs -cat /user/cloudera/departments/part*
+$ hdfs dfs -cat /user/cloudera/question88/departments/part*
--- big-data-developer-with-apache-hadoop/training-cert-exercises-4/question89.scala
+++ big-data-developer-with-apache-hadoop/training-cert-exercises-4/question89.scala
@@ -1,33 +1,29 @@
 /** Question 89
- * Problem Scenario 29 : Please accomplish the following exercises using HDFS command line options.
- * 1. Create a directory in hdfs named hdfs_commands.
- * 2. Create a file in hdfs named data.txt in hdfs_commands.
- * 3. Now copy this data.txt file on local filesystem, however while copying file please make sure file properties are not changed e.g. file permissions.
- * 4. Now create a file in local directory named data_local.txt and move this file to hdfs in hdfs_commands directory.
- * 5. Create a file data_hdfs.txt in hdfs_commands directory and copy it to local file system.
- * 6. Create a file in local filesystem named file1.txt and put it to hdfs
- */
-
-//Answer : See the explanation for Step by Step Solution and configuration.
-
-//Explanation: Solution : 
-//Step 1 : Create directory 
-$ hdfs dfs -mkdir hdfs_commands 
-
-//Step 2 : Create a file in hdfs named data.txt in hdfs_commands. 
-$ hdfs dfs -touchz hdfs_commands/data.txt 
-
-//Step 3 : Now copy this data.txt file on local filesystem, however while copying file please make sure file properties are not changed e.g. file permissions. 
-$ hdfs dfs -copyToLocal -p hdfs_commands/data.txt /home/cloudera/Desktop/HadoopExam 
-
-//Step 4 : Now create a file in local directory named data_local.txt and move this file to hdfs in hdfs_commands directory. 
-$ touch data_local.txt 
-$ hdfs dfs -moveFromLocal /home/cloudera/Desktop/HadoopExam/data_local.txt hdfs_commands/ 
-
-//Step 5 : Create a file data_hdfs.txt in hdfs_commands directory and copy it to local file system. 
-$ hdfs dfs -touchz hdfs_commands/data_hdfs.txt 
-$ hdfs dfs -get hdfs_commands/data_hdfs.txt /home/cloudera/Desktop/HadoopExam/ 
-
-//Step 6 : Create a file in local filesystem named filel .txt and put it to hdfs 
-$ touch file1.txt 
-$ hdfs dfs -put/home/cloudera/Desktop/HadoopExam/file1.txt hdfs_commands/
+  * Problem Scenario 29 : Please accomplish the following exercises using HDFS command line options.
+  * 1. Create a directory in hdfs named hdfs_commands.
+  * 2. Create a file in hdfs named data.txt in hdfs_commands.
+  * 3. Now copy this data.txt file on local filesystem, however while copying file please make sure file properties are not changed e.g. file permissions.
+  * 4. Now create a file in local directory named data_local.txt and move this file to hdfs in hdfs_commands directory.
+  * 5. Create a file data_hdfs.txt in hdfs_commands directory and copy it to local file system.
+  * 6. Create a file in local filesystem named file1.txt and put it to hdfs
+  */
+// 1. Create a directory in hdfs named hdfs_commands.
+$ hdfs dfs -mkdir /user/cloudera/hdfs_commands
+// 2. Create a file in hdfs named data.txt in hdfs_commands.
+$ hdfs dfs -touchz /user/cloudera/hdfs_commands/data.txt
+// 3. Now copy this data.txt file on local filesystem, however while copying file please make sure file properties are not changed e.g. file permissions.
+$ mkdir /home/cloudera/hdfs_commands
+  $ hdfs dfs -get -p /user/cloudera/hdfs_commands/data.txt /home/cloudera/hdfs_commands
+$ ls -ltr /home/cloudera/hdfs_commands
+// 4. Now create a file in local directory named data_local.txt and move this file to hdfs in hdfs_commands directory.
+$ touch /home/cloudera/hdfs_commands/data_local.txt
+$ hdfs dfs -put /home/cloudera/hdfs_commands/data_local.txt /user/cloudera/hdfs_commands
+// 5. Create a file data_hdfs.txt in hdfs_commands directory and copy it to local file system.
+$ hdfs dfs -touchz /user/cloudera/hdfs_commands/data_hdfs.txt
+$ hdfs dfs -get /user/cloudera/hdfs_commands/data_hdfs.txt  /home/cloudera/hdfs_commands
+// 6. Create a file in local filesystem named file1.txt and put it to hdfs
+$ touch /home/cloudera/hdfs_commands/file1.txt
+$ hdfs dfs -put /home/cloudera/hdfs_commands/file1.txt /user/cloudera/hdfs_commands
+// Check local directory and hdfs directory
+$ ls -ltr /home/cloudera/hdfs_commands
+$ hdfs dfs -ls /user/cloudera/hdfs_commands
