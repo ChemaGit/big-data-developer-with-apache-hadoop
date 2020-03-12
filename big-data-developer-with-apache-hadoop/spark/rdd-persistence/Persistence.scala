9db8befebaecfde95520ea56a16607a186cbdd7f
Chema <josem32832@gmail.com>
2020-03-12 09:57:28 +0100

    20200312

--- big-data-developer-with-apache-hadoop/spark/rdd-persistence/Persistence.scala
+++ big-data-developer-with-apache-hadoop/spark/rdd-persistence/Persistence.scala
@@ -1,11 +1,45 @@
-  /** 
-   * RDD Persistence
-   */
-  val mydata = sc.textFile("file:/home/training/training_materials/devsh/examples/example-data/purplecow.txt")
-  val myrdd1 = mydata.map(s => s.toUpperCase())
-  myrdd1.toDebugString
-  myrdd1.count()
-  myrdd1.persist()
-  val myrdd2 = myrdd1.filter(s => s.startsWith("I"))
-  myrdd2.count()
-  myrdd2.toDebugString
+/**
+ * RDD Persistence
+ */
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object Temperatures1800 {
+
+  val spark = SparkSession
+    .builder()
+    .appName("question54")
+    .master("local[*]")
+    .config("spark.sql.shuffle.partitions", "4") //Change to a more reasonable default number of partitions for our data
+    .config("spark.app.id", "question54") // To silence Metrics warning
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
+  def main(args: Array[String]): Unit = {
+
+    Logger.getRootLogger.setLevel(Level.ERROR)
+
+    try {
+
+      val mydata = sc.textFile("file:/home/training/training_materials/devsh/examples/example-data/purplecow.txt")
+      val myrdd1 = mydata.map(s => s.toUpperCase())
+      myrdd1.toDebugString
+      myrdd1.count()
+      myrdd1.persist()
+      val myrdd2 = myrdd1.filter(s => s.startsWith("I"))
+      myrdd2.count()
+      myrdd2.toDebugString
+
+      // To have the opportunity to view the web console of Spark: http://localhost:4040/
+      println("Type whatever to the console to exit......")
+      scala.io.StdIn.readLine()
+    } finally {
+      sc.stop()
+      println("SparkContext stopped.")
+      spark.stop()
+      println("SparkSession stopped.")
+    }
+  }
+}
--- big-data-developer-with-apache-hadoop/spark/rdd-persistence/Persistence2.scala
+++ big-data-developer-with-apache-hadoop/spark/rdd-persistence/Persistence2.scala
@@ -1,32 +1,65 @@
   /** 
    * RDD Persistence 2
    */
-  // Count web server log requests by user id
-  val userReqs = sc.textFile("/loudacre/weblogs/*2.log").
-     map(line => line.split(' ')).
-     map(words => (words(2),1)).  
-     reduceByKey((v1,v2) => v1 + v2)
-   
-  // Map account data to (userid,"lastname,firstname") pairs
-  val accounts = sc.textFile("/loudacre/accounts/*").
-    map(line => line.split(',')).
-    map(values => (values(0),values(4) + ',' + values(3)))
+  import org.apache.log4j.{Level, Logger}
+  import org.apache.spark.sql.SparkSession
 
-  // Join account names with request counts
-  val accountHits = accounts.join(userReqs).map(pair => pair._2)
+  object Persistence2 {
 
-  accountHits.filter(pair => pair._2 > 5).count()
+    val spark = SparkSession
+      .builder()
+      .appName("Persistence2")
+      .master("local[*]")
+      .config("spark.sql.shuffle.partitions", "4") //Change to a more reasonable default number of partitions for our data
+      .config("spark.app.id", "Persistence2") // To silence Metrics warning
+      .getOrCreate()
 
-  accountHits.persist()
+    val sc = spark.sparkContext
 
-  accountHits.filter(pair => pair._2 > 5).count()
-  accountHits.toDebugString
+    def main(args: Array[String]): Unit = {
 
-  accountHits.unpersist()
+      Logger.getRootLogger.setLevel(Level.ERROR)
 
-  import org.apache.spark.storage.StorageLevel._
+      try {
 
-  accountHits.persist(DISK_ONLY)
+        // Count web server log requests by user id
+        val userReqs = sc.textFile("/loudacre/weblogs/*2.log").
+          map(line => line.split(' ')).
+          map(words => (words(2),1)).
+          reduceByKey((v1,v2) => v1 + v2)
 
-  accountHits.filter(pair => pair._2 > 5).count()
-  accountHits.toDebugString
+        // Map account data to (userid,"lastname,firstname") pairs
+        val accounts = sc.textFile("/loudacre/accounts/*").
+          map(line => line.split(',')).
+          map(values => (values(0),values(4) + ',' + values(3)))
+
+        // Join account names with request counts
+        val accountHits = accounts.join(userReqs).map(pair => pair._2)
+
+        accountHits.filter(pair => pair._2 > 5).count()
+
+        accountHits.persist()
+
+        accountHits.filter(pair => pair._2 > 5).count()
+        accountHits.toDebugString
+
+        accountHits.unpersist()
+
+        import org.apache.spark.storage.StorageLevel._
+
+        accountHits.persist(DISK_ONLY)
+
+        accountHits.filter(pair => pair._2 > 5).count()
+        accountHits.toDebugString
+
+        // To have the opportunity to view the web console of Spark: http://localhost:4040/
+        println("Type whatever to the console to exit......")
+        scala.io.StdIn.readLine()
+      } finally {
+        sc.stop()
+        println("SparkContext stopped.")
+        spark.stop()
+        println("SparkSession stopped.")
+      }
+    }
+  }
